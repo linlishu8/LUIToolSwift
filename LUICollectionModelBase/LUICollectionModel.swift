a96b11acb6ab60295d3d72ac3b7d3cc06f7e062c
Your Name <you@example.com>
2024-09-22 20:50:02 +0800

    代码提交

--- LUICollectionModelBase/LUICollectionModel.swift
+++ LUICollectionModelBase/LUICollectionModel.swift
@@ -19,12 +19,12 @@ class LUICollectionModel: LUICollectionModelObjectBase {
     
     //返回所有的单元格数据
     var allCellModels: [LUICollectionCellModel] {
-            var cells: [LUICollectionCellModel] = []
-            for section in sectionModels {
-                cells.append(contentsOf: section.cellModels)
-            }
-            return cells
+        var cells: [LUICollectionCellModel] = []
+        for section in sectionModels {
+            cells.append(contentsOf: section.cellModels)
         }
+        return cells
+    }
     var userInfo: Any?//自定義的擴展對象
     
     //分组数量
@@ -91,13 +91,218 @@ class LUICollectionModel: LUICollectionModelObjectBase {
     }
     
     func insertCellModel(_ cellModel: LUICollectionCellModel, atIndexPath indexPath: IndexPath) {
-        if let sectionModel = sectionModelAtIndex(indexPath.section) {
-            sectionModel.insertCellModel(cellModel, atIndex: indexPath.row)
+        guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
+        sectionModel.insertCellModel(cellModel, atIndex: indexPath.row)
+    }
+    
+    func insertCellModels(_ cellModels: [LUICollectionCellModel], afterIndexPath indexPath: IndexPath) {
+        guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
+        sectionModel.insertCellModels(cellModels, afterIndex: indexPath.row)
+    }
+    
+    func insertCellModels(_ cellModels: [LUICollectionCellModel], beforeIndexPath indexPath: IndexPath) {
+        guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
+        sectionModel.insertCellModels(cellModels, beforeIndex: indexPath.row)
+    }
+    
+    func insertCellModelsToBottom(_ cellModels: [LUICollectionCellModel]) {
+        guard let sectionModel = self.sectionModels.last else { return }
+        sectionModel.insertCellModelsToBottom(cellModels)
+    }
+    
+    func insertCellModelsToTop(_ cellModels: [LUICollectionCellModel]) {
+        guard let sectionModel = self.sectionModels.first else { return }
+        sectionModel.insertCellModelsToTop(cellModels)
+    }
+    
+    func removeCellModel(_ cellModel: LUICollectionCellModel) {
+        for sectionModel in sectionModels {
+            sectionModel.removeCellModel(cellModel: cellModel)
         }
     }
-
+    
+    func removeCellModels(_ cellModels: [LUICollectionCellModel]) {
+        for cellModel in cellModels {
+            removeCellModel(cellModel)
+        }
+    }
+    
+    func removeCellModelAtIndexPath(_ indexPath: IndexPath) {
+        guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
+        sectionModel .removeCellModelAtIndex(indexPath.row)
+    }
+    
+    func removeCellModelsAtIndexPaths(_ indexPaths: [IndexPath]) {
+        var sectionMap = [Int: [IndexPath]]()
+        for indexPath in indexPaths {
+            let section = indexPath.section
+            if sectionMap[section] == nil {
+                sectionMap[section] = [indexPath]
+            } else {
+                sectionMap[section]?.append(indexPath)
+            }
+        }
+        for (section, subIndexPaths) in sectionMap {
+            let indexSet = IndexSet(subIndexPaths.map { $0.row })
+            if let sectionModel = sectionModelAtIndex(section) {
+                sectionModel.removeCellModelsAtIndexes(indexes: indexSet)
+            }
+        }
+    }
+    
     func addSectionModel(_ sectionModel: LUICollectionSectionModel) {
         sectionModel.collectionModel = self;
         self.sectionModels.append(sectionModel)
+    }
+    
+    func insertSectionModel(_ sectionModel: LUICollectionSectionModel, atIndex index: Int) {
+        sectionModel.collectionModel = self
+        sectionModels.insert(sectionModel, at: index)
+    }
+    
+    func addSectionModels(_ sectionModels: [LUICollectionSectionModel]) {
+        for sectionModel in sectionModels {
+            addSectionModel(sectionModel)
+        }
+    }
+    
+    func removeSectionModel(_ sectionModel: LUICollectionSectionModel) {
+        sectionModel.collectionModel = nil
+        if let index = sectionModels.firstIndex(where: { $0 == sectionModel} ) {
+            removeSectionModelAtIndex(index)
+        }
+    }
+    
+    func removeSectionModelAtIndex(_ index: Int) {
+        if index > 0 && index < self.sectionModels.count {
+            sectionModels.remove(at: index)
+        }
+    }
+    
+    func removeSectionModelsInRange(_ range: Range<Int>) {
+        sectionModels.removeSubrange(range)
+    }
+    
+    func removeAllSectionModels() {
+        sectionModels.removeAll()
+    }
+    
+    func indexPathOfCellModel(_ cellModel: LUICollectionCellModel) -> IndexPath? {
+        for (idx, section) in sectionModels.enumerated() {
+            if let row = section.indexOfCellModel(cellModel), row != NSNotFound {
+                return IndexPath(row: row, section: idx)
+            }
+        }
+        return nil
+    }
+    
+    func enumerateCellModelsUsingBlock(_ block: (LUICollectionCellModel, IndexPath, inout Bool) -> Void) {
+        var stop = false
+        for (sectionIndex, section) in sectionModels.enumerated() {
+            for (itemIndex, cellModel) in section.cellModels.enumerated() {
+                let indexPath = IndexPath(item: itemIndex, section: sectionIndex)
+                block(cellModel, indexPath, &stop)
+                if stop { return }
+            }
+        }
+    }
+    
+    func indexPathOfCellModelPassingTest(_ block: (LUICollectionCellModel, IndexPath, inout Bool) -> Bool) -> IndexPath? {
+        var result: IndexPath?
+        enumerateCellModelsUsingBlock { cellModel, indexpath, stop in
+            if block(cellModel, indexpath, &stop) {
+                result = indexpath
+                stop = true
+            }
+        }
+        return result
+    }
+    
+    func indexPathsOfCellModels(_ cellModels: [LUICollectionCellModel]) -> [IndexPath] {
+        var indexPaths: [IndexPath] = []
+        for cellModel in cellModels {
+            if let indexPath = indexPathOfCellModel(cellModel) {
+                indexPaths.append(indexPath)
+            }
+        }
+        return indexPaths
+    }
+    
+    func indexOfSectionModel(_ sectionModel: LUICollectionSectionModel) -> Int? {
+        return sectionModels.firstIndex(of: sectionModel)
+    }
+    
+    func indexOfSectionModel(_ sectionModel: LUICollectionSectionModel) -> NSIndexSet? {
+        if let index = sectionModels.firstIndex(of: sectionModel) {
+            return NSIndexSet(index: index)
+        }
+        return nil
+    }
+    
+    func cellModelAtIndexPath(_ indexPath: IndexPath) -> LUICollectionCellModel? {
+        if let section = sectionModelAtIndex(indexPath.section), let cell = section.cellModelAtIndex(indexPath.row) {
+            return cell
+        }
+        return nil
+    }
+    
+    func cellModelsAtIndexPaths(_ indexPaths: [IndexPath]) -> [LUICollectionCellModel] {
+        var cellModels: [LUICollectionCellModel] = []
+        for indexPath in indexPaths {
+            if let cm = cellModelAtIndexPath(indexPath) {
+                cellModels.append(cm)
+            }
+        }
+        return cellModels
+    }
+    
+    func emptySectionModels() -> [LUICollectionSectionModel] {
+        var models: [LUICollectionSectionModel] = []
+        for sectionModel in sectionModels {
+            if sectionModel.numberOfCells == 0 {
+                models.append(sectionModel)
+            }
+        }
+        return models
+    }
+    
+    func removeEmptySectionModels() {
+        let models = emptySectionModels()
+        sectionModels.removeAll { model in
+            models.contains(where: { $0 === model })
+        }
+    }
+    
+    func moveCellModelAtIndexPath(_ sourceIndexPath: IndexPath, toIndexPath destinationIndexPath: IndexPath) {
+        guard let sourceCellModel = cellModelAtIndexPath(sourceIndexPath) else { return }
+        removeCellModelAtIndexPath(sourceIndexPath)
+        insertCellModel(sourceCellModel, atIndexPath: destinationIndexPath)
+    }
+    
+    func selectCellModel(_ cellModel: LUICollectionCellModel) {
+        guard allowsSelection else { cellModel.selected = true; return }
+        guard !allowsMultipleSelection else { return }
+        for allCellModel in allCellModels {
+            if allCellModel.isEqual(cellModel) {
+                allCellModel.selected = false
+            }
+        }
+    }
+    
+    func selectCellModels(_ cellModels: [LUICollectionCellModel]) {
+        guard allowsSelection else { return }
+        guard allowsMultipleSelection else { return }
+        for cellModel in cellModels {
+            cellModel.selected = true
+        }
+    }
+    
+    func selectCellModelAtIndexPath(_ indexPath: IndexPath) {
+        guard let cellModel = cellModelAtIndexPath(indexPath) else { return }
+        selectCellModel(cellModel)
+    }
+    
+    func selectCellModelsAtIndexPaths(_ indexPaths: [IndexPath]) {
+        selectCellModels(cellModelsAtIndexPaths(indexPaths))
     }
 }
