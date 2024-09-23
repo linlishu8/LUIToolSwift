1b63af873dcaa350e039191517a4b649183d61e4
liuwei <linlishu8@gamil.com>
2024-09-23 10:34:56 +0800

    代码提交

--- LUICollectionModelBase/LUICollectionModel.swift
+++ LUICollectionModelBase/LUICollectionModel.swift
@@ -60,10 +60,12 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return self.sectionModels[index]
     }
     
+    //产生一个空的分组
     func createEmptySectionModel() -> LUICollectionSectionModel {
         return LUICollectionSectionModel()
     }
     
+    //快速加到单元格到最後一個分组中
     func addCellModel(_ cellModel: LUICollectionCellModel) {
         guard let section = sectionModels.last else {
             let newSection = createEmptySectionModel()
@@ -74,6 +76,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         section.addCellModel(cellModel)
     }
     
+    //快速加到单元格到第一個分组的第一个位置上
     func addCellModelToFirst(_ cellModel: LUICollectionCellModel) {
         guard let section = sectionModels.last else {
             let newSection = createEmptySectionModel()
@@ -84,12 +87,14 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         section.insertCellModel(cellModel, atIndex: 0)
     }
     
+    //快速添加多个单元格到最后一个分组中,如果分组不存在,会创建分组
     func addCellModels(_ cellModels:[LUICollectionCellModel]) {
         for LUICollectionCellModel in cellModels {
             addCellModel(LUICollectionCellModel)
         }
     }
     
+    //插入某个单元格到指定的位置
     func insertCellModel(_ cellModel: LUICollectionCellModel, atIndexPath indexPath: IndexPath) {
         guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
         sectionModel.insertCellModel(cellModel, atIndex: indexPath.row)
@@ -105,33 +110,39 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         sectionModel.insertCellModels(cellModels, beforeIndex: indexPath.row)
     }
     
+    //添加单元格到底部
     func insertCellModelsToBottom(_ cellModels: [LUICollectionCellModel]) {
         guard let sectionModel = self.sectionModels.last else { return }
         sectionModel.insertCellModelsToBottom(cellModels)
     }
     
+    //添加单元格到顶部
     func insertCellModelsToTop(_ cellModels: [LUICollectionCellModel]) {
         guard let sectionModel = self.sectionModels.first else { return }
         sectionModel.insertCellModelsToTop(cellModels)
     }
     
+    //将cell从所有的分组中移除
     func removeCellModel(_ cellModel: LUICollectionCellModel) {
         for sectionModel in sectionModels {
             sectionModel.removeCellModel(cellModel: cellModel)
         }
     }
     
+    //将多个cell从所有的分组中移除
     func removeCellModels(_ cellModels: [LUICollectionCellModel]) {
         for cellModel in cellModels {
             removeCellModel(cellModel)
         }
     }
     
+    //移除指定位置上的单元格
     func removeCellModelAtIndexPath(_ indexPath: IndexPath) {
         guard let sectionModel = sectionModelAtIndex(indexPath.section) else { return }
         sectionModel .removeCellModelAtIndex(indexPath.row)
     }
     
+    //将多个cell从所有的分组中移除
     func removeCellModelsAtIndexPaths(_ indexPaths: [IndexPath]) {
         var sectionMap = [Int: [IndexPath]]()
         for indexPath in indexPaths {
@@ -150,6 +161,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         }
     }
     
+    //添加分组模型数据
     func addSectionModel(_ sectionModel: LUICollectionSectionModel) {
         sectionModel.collectionModel = self;
         self.sectionModels.append(sectionModel)
@@ -166,6 +178,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         }
     }
     
+    //移除掉分组模型数据
     func removeSectionModel(_ sectionModel: LUICollectionSectionModel) {
         sectionModel.collectionModel = nil
         if let index = sectionModels.firstIndex(where: { $0 == sectionModel} ) {
@@ -173,6 +186,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         }
     }
     
+    //移除掉分组模型数据
     func removeSectionModelAtIndex(_ index: Int) {
         if index > 0 && index < self.sectionModels.count {
             sectionModels.remove(at: index)
@@ -183,10 +197,12 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         sectionModels.removeSubrange(range)
     }
     
+    //清空所有数据
     func removeAllSectionModels() {
         sectionModels.removeAll()
     }
     
+    //获取指定单元格的位置
     func indexPathOfCellModel(_ cellModel: LUICollectionCellModel) -> IndexPath? {
         for (idx, section) in sectionModels.enumerated() {
             if let row = section.indexOfCellModel(cellModel), row != NSNotFound {
@@ -196,6 +212,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return nil
     }
     
+    //遍历所有的单元格
     func enumerateCellModelsUsingBlock(_ block: (LUICollectionCellModel, IndexPath, inout Bool) -> Void) {
         var stop = false
         for (sectionIndex, section) in sectionModels.enumerated() {
@@ -207,6 +224,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         }
     }
     
+    //查找指定的单元格
     func indexPathOfCellModelPassingTest(_ block: (LUICollectionCellModel, IndexPath, inout Bool) -> Bool) -> IndexPath? {
         var result: IndexPath?
         enumerateCellModelsUsingBlock { cellModel, indexpath, stop in
@@ -218,6 +236,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return result
     }
     
+    //获取多个指定单元格的位置
     func indexPathsOfCellModels(_ cellModels: [LUICollectionCellModel]) -> [IndexPath] {
         var indexPaths: [IndexPath] = []
         for cellModel in cellModels {
@@ -228,10 +247,10 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return indexPaths
     }
     
+    //获取指定分组的索引值
     func indexOfSectionModel(_ sectionModel: LUICollectionSectionModel) -> Int? {
         return sectionModels.firstIndex(of: sectionModel)
     }
-    
     func indexSetOfSectionModel(_ sectionModel: LUICollectionSectionModel) -> NSIndexSet? {
         if let index = sectionModels.firstIndex(of: sectionModel) {
             return NSIndexSet(index: index)
@@ -239,13 +258,13 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return nil
     }
     
+    //返回indexpath指定的单元格数据
     func cellModelAtIndexPath(_ indexPath: IndexPath) -> LUICollectionCellModel? {
         if let section = sectionModelAtIndex(indexPath.section), let cell = section.cellModelAtIndex(indexPath.row) {
             return cell
         }
         return nil
     }
-    
     func cellModelsAtIndexPaths(_ indexPaths: [IndexPath]) -> [LUICollectionCellModel] {
         var cellModels: [LUICollectionCellModel] = []
         for indexPath in indexPaths {
@@ -266,6 +285,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         return models
     }
     
+    //移除掉没有单元格的空分组
     func removeEmptySectionModels() {
         let models = emptySectionModels()
         sectionModels.removeAll { model in
@@ -273,6 +293,7 @@ class LUICollectionModel: LUICollectionModelObjectBase {
         }
     }
     
+    //移动单元格
     func moveCellModelAtIndexPath(_ sourceIndexPath: IndexPath, toIndexPath destinationIndexPath: IndexPath) {
         guard let sourceCellModel = cellModelAtIndexPath(sourceIndexPath) else { return }
         removeCellModelAtIndexPath(sourceIndexPath)
