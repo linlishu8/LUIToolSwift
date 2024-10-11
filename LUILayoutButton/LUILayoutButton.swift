648c417e5f3ab86e3bd39aa4cc740e8adc1c08f9
liuwei <linlishu8@gamil.com>
2024-10-11 11:02:37 +0800

    代码提交

--- LUILayoutButton/LUILayoutButton.swift
+++ LUILayoutButton/LUILayoutButton.swift
@@ -13,27 +13,100 @@ public enum LUILayoutButtonContentStyle: Int {
 }
 
 public class LUILayoutButton: UIButton {
+    private lazy var flowlayout: LUIFlowLayoutConstraint = {
+        let layout = LUIFlowLayoutConstraint.init()
+        layout.layoutHiddenItem = true
+        layout.layoutDirection = .constraintHorizontal
+        return layout
+    }()
     open var contentStyle: LUILayoutButtonContentStyle = .horizontal //图标和文字的位置，默认横着放
     open var interitemSpacing: Int = 3 //图标与文字之间的间距,默认是3px
     open var reverseContent: Bool = false //是否逆转图标与文字的顺序,默认是NO:图标在左/上,文本在右/下
     open var imageSize: CGSize = .zero //imageView尺寸,如果某一边为0，代表不限制。默认为(0,0),代表自动根据图片大小计算
     open var hideImageViewForNoImage: Bool = false //当没有图片时，是否隐藏imageView。默认为NO，不隐藏imageView，如果imageSize不为(0,0),imageView将继续占用空间
     open var layoutVerticalAlignment: LUILayoutConstraintVerticalAlignment = .verticalCenter //所有元素作为一个整体,在垂直方向上的位置,以及每一个元素在整体内的垂直方向上的对齐方式.默认为LUILayoutConstraintVerticalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
-    open var layoutHorizontalAlignment: LUILayoutConstraintHorizontalAlignment = .horizontalCenter //所有元素作为一个整体,在水平方向上的位置,以及每一个元素在整体内的水平方向上的对方方式.默认为LUILayoutConstraintHorizontalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
+    open var layoutHorizontalAlignment: LUILayoutConstraintHorizontalAlignment {
+        get {
+            return self.flowlayout.layoutHorizontalAlignment
+        }
+        set {
+            if self.flowlayout.layoutHorizontalAlignment == newValue { return }
+            self.flowlayout.layoutHorizontalAlignment = layoutHorizontalAlignment
+            self.setNeedsLayout()
+        }
+    } //所有元素作为一个整体,在水平方向上的位置,以及每一个元素在整体内的水平方向上的对方方式.默认为LUILayoutConstraintHorizontalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
     open var contentInsets: UIEdgeInsets = .zero //内边距,默认为(0,0,0,0)
     open var minHitSize: CGSize = CGSize(width: 30, height: 30)//最小的点击区域尺寸，通过设置该值，可以使得点击区域大于self.bounds(当minHitSize>bounds.size时)。默认为30x30
+    
+    private lazy var sizeFitTitleLabel: UILabel = {
+        guard let labelType = titleLabel?.classForCoder as? UILabel.Type else {
+            return UILabel()
+        }
+        let label = labelType.init()
+        configureLabel(label)
+        return label
+    }()
+
+    private func configureLabel(_ label: UILabel) {
+        guard let titleLabel = titleLabel else { return }
+        
+        label.attributedText = nil
+        label.text = nil
+        label.contentMode = titleLabel.contentMode
+        label.font = titleLabel.font
+        label.shadowOffset = titleLabel.shadowOffset
+        label.textAlignment = titleLabel.textAlignment
+        label.lineBreakMode = titleLabel.lineBreakMode
+        label.isHighlighted = titleLabel.isHighlighted
+        label.isEnabled = titleLabel.isEnabled
+        label.numberOfLines = titleLabel.numberOfLines
+        label.adjustsFontSizeToFitWidth = titleLabel.adjustsFontSizeToFitWidth
+        label.baselineAdjustment = titleLabel.baselineAdjustment
+        label.minimumScaleFactor = titleLabel.minimumScaleFactor
+        label.allowsDefaultTighteningForTruncation = titleLabel.allowsDefaultTighteningForTruncation
+        // label.lineBreakStrategy = titleLabel.lineBreakStrategy // Uncomment if needed and available in your iOS version
+        label.preferredMaxLayoutWidth = titleLabel.preferredMaxLayoutWidth
+    }
+    
+    private lazy var sizeFitImageView: UIImageView = {
+        let imageViewType = self.imageView?.classForCoder as? UIImageView.Type ?? UIImageView.self
+        let imageView = imageViewType.init()
+        configureImageView(imageView)
+        return imageView
+    }()
+
+    private func configureImageView(_ imageView: UIImageView) {
+        guard let originalImageView = self.imageView else { return }
+        
+        imageView.image = nil
+        imageView.contentMode = originalImageView.contentMode
+    }
     
     public init(contentStyle: LUILayoutButtonContentStyle) {
         super.init(frame: .zero)
         self.contentStyle = contentStyle
+        self.setupButton()
     }
-    
-    lazy private var sizeFitTitleLabel: UILabel = {
-        let titleLabel = self.titleLabel
-        return titleLabel ?? UILabel()
-    }()
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupButton() {
+        self.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    public override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        if !self.isUserInteractionEnabled || self.isHidden || !self.isEnabled {
+            return false
+        }
+        let minHitSize = self.minHitSize
+        var bounds = self.bounds
+        let center = CGPoint(x: CGRectGetMidX(bounds), y: CGRectGetMidY(bounds))
+        bounds.size.width = max(bounds.size.width, minHitSize.width)
+        bounds.size.height = max(bounds.size.height, minHitSize.height)
+        bounds.origin.x = center.x - bounds.size.width / 2
+        bounds.origin.y = center.y - bounds.size.height / 2
+        return CGRectContainsPoint(bounds, point)
     }
 }
