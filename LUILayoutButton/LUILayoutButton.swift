f772f8f599f627551cb82cd31f202eec48aab7cf
liuwei <linlishu8@gamil.com>
2024-10-11 11:15:22 +0800

    代码提交

--- LUILayoutButton/LUILayoutButton.swift
+++ LUILayoutButton/LUILayoutButton.swift
@@ -19,23 +19,69 @@ public class LUILayoutButton: UIButton {
         layout.layoutDirection = .constraintHorizontal
         return layout
     }()
-    open var contentStyle: LUILayoutButtonContentStyle = .horizontal //图标和文字的位置，默认横着放
-    open var interitemSpacing: Int = 3 //图标与文字之间的间距,默认是3px
+    private var _contentStyle: LUILayoutButtonContentStyle = .horizontal //图标和文字的位置，默认横着放
+    open var contentStyle: LUILayoutButtonContentStyle {
+        get {
+            return _contentStyle
+        }
+        set {
+            if _contentStyle == newValue { return }
+            _contentStyle = newValue
+            switch _contentStyle {
+            case .horizontal:
+                self.flowlayout.layoutDirection = .constraintHorizontal
+            case .vertical:
+                self.flowlayout.layoutDirection = .constraintVertical
+            }
+            self.setNeedsLayout()
+        }
+    }
+    //图标与文字之间的间距,默认是3px
+    open var interitemSpacing: CGFloat {
+        get {
+            return self.flowlayout.interitemSpacing
+        }
+        set {
+            if self.interitemSpacing == newValue { return }
+            self.flowlayout.interitemSpacing = newValue
+            self.setNeedsLayout()
+        }
+    }
     open var reverseContent: Bool = false //是否逆转图标与文字的顺序,默认是NO:图标在左/上,文本在右/下
     open var imageSize: CGSize = .zero //imageView尺寸,如果某一边为0，代表不限制。默认为(0,0),代表自动根据图片大小计算
     open var hideImageViewForNoImage: Bool = false //当没有图片时，是否隐藏imageView。默认为NO，不隐藏imageView，如果imageSize不为(0,0),imageView将继续占用空间
-    open var layoutVerticalAlignment: LUILayoutConstraintVerticalAlignment = .verticalCenter //所有元素作为一个整体,在垂直方向上的位置,以及每一个元素在整体内的垂直方向上的对齐方式.默认为LUILayoutConstraintVerticalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
+    open var layoutVerticalAlignment: LUILayoutConstraintVerticalAlignment {
+        get {
+            return self.flowlayout.layoutVerticalAlignment
+        }
+        set {
+            if self.flowlayout.layoutVerticalAlignment == newValue { return }
+            self.flowlayout.layoutVerticalAlignment = newValue
+            self.setNeedsLayout()
+        }
+    } //所有元素作为一个整体,在垂直方向上的位置,以及每一个元素在整体内的垂直方向上的对齐方式.默认为LUILayoutConstraintVerticalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
     open var layoutHorizontalAlignment: LUILayoutConstraintHorizontalAlignment {
         get {
             return self.flowlayout.layoutHorizontalAlignment
         }
         set {
             if self.flowlayout.layoutHorizontalAlignment == newValue { return }
-            self.flowlayout.layoutHorizontalAlignment = layoutHorizontalAlignment
+            self.flowlayout.layoutHorizontalAlignment = newValue
             self.setNeedsLayout()
         }
     } //所有元素作为一个整体,在水平方向上的位置,以及每一个元素在整体内的水平方向上的对方方式.默认为LUILayoutConstraintHorizontalAlignmentCenter.详细查看LUIFlowLayoutConstraint.h
-    open var contentInsets: UIEdgeInsets = .zero //内边距,默认为(0,0,0,0)
+    
+    //内边距,默认为(0,0,0,0)
+    open var contentInsets: UIEdgeInsets {
+        get {
+            self.flowlayout.contentInsets
+        }
+        set {
+            if UIEdgeInsetsEqualToEdgeInsets(self.contentInsets, newValue) { return }
+            self.flowlayout.contentInsets = newValue
+            self.setNeedsLayout()
+        }
+    }
     open var minHitSize: CGSize = CGSize(width: 30, height: 30)//最小的点击区域尺寸，通过设置该值，可以使得点击区域大于self.bounds(当minHitSize>bounds.size时)。默认为30x30
     
     private lazy var sizeFitTitleLabel: UILabel = {
